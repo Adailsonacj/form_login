330f204d6d8de03726a3c9ccac4db166023876ce
adailsonacj <adailsonacj@live.com>
2019-02-19 15:14:56 -0300

    Fazendo autenticação com JavaScript

--- login.js
+++ login.js
@@ -1,6 +1,15 @@
 
-function autenticar() {
+   function autenticar() {
+ 	registered = {
+ 		user: 'adailsonacj@live.com', 
+ 		pass: 123123 		
+ 	}
+
+ 	var user = document.getElementById('user').value 
+ 	var pass = document.getElementById('pass').value
+ 	if(user == registered.user && pass == registered.pass){
+ 		alert("Usuário Logado!")
+ 	}
+ }
+
   
-        alert("zxcz")
-    
-}
