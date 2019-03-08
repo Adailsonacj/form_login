ad3639b90cf07a515094c6a6b14193ec3382bff3
adailsonacj <adailsonacj@live.com>
2019-03-08 17:07:21 -0300

    recebendo users do webservice

--- login.js
+++ login.js
@@ -4,6 +4,8 @@ function autenticar(form) {
 		user: 'adailsonacj@live.com',
 		pass: 123
 	}
+	var posts = await fetch('https://calm-coast-69939.herokuapp.com/getuser')
+	
 
 	var user = form.user.value
 	var pass = form.pass.value
